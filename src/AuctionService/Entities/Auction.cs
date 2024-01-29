2c6d1a6737165e5beaa7345328b95cbed3942cb1
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-29 23:18:59 +0500

    fix: fixed auctionend typo and updated field modifiers

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -4,17 +4,15 @@ namespace AuctionService.Entities;
 
 public class Auction
 {
-    public Guid Id { get; set; }
-    public decimal ReservePrice { get; set; }
+    public required Guid Id { get; set; }
+    public required decimal ReservePrice { get; set; }
     public decimal CurrentHighBid { get; set; }
-    public int? SoldAmount { get; set; }
-    [MaxLength(100)]
-    public string? Seller { get; set; }
-    [MaxLength(100)]
-    public string? Winner { get; set; }
+    public int SoldAmount { get; set; }
+    [MaxLength(100)] public string? Seller { get; set; }
+    [MaxLength(100)] public string? Winner { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    public required DateTime ActionEnd { get; set; }
+    public required DateTime AuctionEnd { get; set; }
     public required Status Status { get; set; }
-    public required Item Item { get; set; }
+    public Item? Item { get; set; }
 }
--- src/AuctionService/Entities/Item.cs
+++ src/AuctionService/Entities/Item.cs
@@ -6,14 +6,14 @@ namespace AuctionService.Entities;
 [Table("Items")]
 public class Item
 {
-    public Guid Id { get; set; }
-    [MaxLength(100)]
-    public required string Name { get; set; }
-    [MaxLength(10000)]
-    public string? Description { get; set; }
-    [MaxLength(1000)]
-    public string? ImageUrl { get; set; }
+    public required Guid Id { get; set; }
 
-    public required Auction Auction { get; set; }
-    public required Guid AuctionId { get; set; }
+    [MaxLength(100)] public required string Name { get; set; }
+
+    [MaxLength(10000)] public required string Description { get; set; }
+
+    [MaxLength(1000)] public string? ImageUrl { get; set; }
+
+    public Auction? Auction { get; set; }
+    public Guid AuctionId { get; set; }
 }
