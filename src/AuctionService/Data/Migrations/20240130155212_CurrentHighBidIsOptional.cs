bdca681cb3d9b1cb06ff5b213697418443b06e8d
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-30 20:53:07 +0500

    feat: add migration to change current high bid to optional

---
+++ src/AuctionService/Data/Migrations/20240130155212_CurrentHighBidIsOptional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CurrentHighBidIsOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentHighBid",
+                table: "Auctions",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentHighBid",
+                table: "Auctions",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+    }
+}
