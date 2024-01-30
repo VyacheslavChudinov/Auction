f7cf70d49702a4e6d2fcfb6b7929bc9066098d60
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-01-30 20:49:27 +0500

    chore: changed spacing

--- src/AuctionService/Entities/Item.cs
+++ src/AuctionService/Entities/Item.cs
@@ -7,13 +7,10 @@ namespace AuctionService.Entities;
 public class Item
 {
     public required Guid Id { get; set; }
-
     [MaxLength(100)] public required string Name { get; set; }
-
     [MaxLength(10000)] public required string Description { get; set; }
-
     [MaxLength(1000)] public string? ImageUrl { get; set; }
-
+    
     public Auction? Auction { get; set; }
     public Guid AuctionId { get; set; }
 }
