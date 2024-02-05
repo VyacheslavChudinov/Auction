3e402c0877cd80fa82768a5cab3ba7c9ff6e0203
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-02-05 22:07:14 +0500

    feat: use polling in case auction service is not available on search service start

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,5 @@
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -5,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -21,3 +23,6 @@ catch (Exception e)
 }
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() => HttpPolicyExtensions.HandleTransientHttpError()
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
