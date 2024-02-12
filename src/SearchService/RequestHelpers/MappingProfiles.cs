2e1674cf996443b8509b094a011d54d3ccd919dc
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-02-12 23:26:27 +0500

    chore: remove redundant mapping

--- src/SearchService/RequestHelpers/MappingProfiles.cs
+++ src/SearchService/RequestHelpers/MappingProfiles.cs
@@ -10,6 +10,5 @@ public class MappingProfiles : Profile
     {
         CreateMap<AuctionCreated, Item>();
         CreateMap<AuctionUpdated, Item>();
-        CreateMap<AuctionDeleted, Item>();
     }
 }
