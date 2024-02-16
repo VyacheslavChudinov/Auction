855fe8f99c2b6ec7d6d984d4b8a95ea2598450c4
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-02-16 19:43:01 +0300

    feat: add register user logic

--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -1,13 +1,51 @@
+using System.Security.Claims;
+using IdentityModel;
+using IdentityService.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityService.Pages.Account.Register;
 
 [SecurityHeaders]
 [AllowAnonymous]
-public class Index : PageModel
+public class Index(UserManager<ApplicationUser> userManager) : PageModel
 {
-    public void OnGet()
+    [BindProperty] public RegisterViewModel Input { get; set; }
+    [BindProperty] public bool RegisterSuccessful { get; set; }
+
+    public IActionResult OnGet(string returnUrl)
     {
+        Input = new RegisterViewModel
+        {
+            ReturnUrl = returnUrl
+        };
+
+        return Page();
+    }
+
+    public async Task<ActionResult> OnPost()
+    {
+        if (Input.Button != "register") return Redirect("~/");
+
+        if (!ModelState.IsValid) return Page();
+        
+        var user = new ApplicationUser()
+        {
+            UserName = Input.UserName,
+            Email = Input.Email,
+            EmailConfirmed = true,
+        };
+
+        var result = await userManager.CreateAsync(user, Input.Password);
+
+        if (!result.Succeeded) return Page();
+        
+        await userManager.AddClaimsAsync(user, new Claim[] { new (JwtClaimTypes.Name, Input.FullName) });
+
+        RegisterSuccessful = true;
+
+        return Page();
     }
 }
